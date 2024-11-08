489ba0d6cec7a75ac076798f70b083b4dba0cd5a
Tom-Jank <tom.jank4@gmail.com>
2024-11-08 12:12:40 +0100

    Updated

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -19,7 +19,7 @@ public class Main {
     var struct = new PDStructureElement(StandardStructureTypes.SECT, sec1);
 
     var a = formBuilder.drawElement(
-        new Cell("SBM Antragzusammenfastung", new Color(229, 229, 229), Color.BLUE.darker(),
+        new Cell("Heading", new Color(229, 229, 229), Color.BLUE.darker(),
             12, formBuilder.PAGE_WIDTH - 100, PDConstants.LEFT_ALIGN),
         50, 25, 50, struct, StandardStructureTypes.H1, 0);
 
@@ -29,31 +29,10 @@ public class Main {
         50, 600, 50, struct, StandardStructureTypes.P, 0);
 
     var header = formBuilder.drawElement(
-        new Cell("Title", new Color(229, 229, 229), Color.BLUE.darker(),
+        new Cell("Page title", new Color(229, 229, 229), Color.BLUE.darker(),
             12, formBuilder.PAGE_WIDTH - 100, PDConstants.LEFT_ALIGN),
         250, 50, 50, sec1, StandardStructureTypes.H2, 0);
 
-
-    var section_title = formBuilder.drawElement(
-        new Cell("Section", new Color(229, 229, 229), Color.BLUE.darker(),
-            12, formBuilder.PAGE_WIDTH - 100, PDConstants.LEFT_ALIGN),
-        150, 75, 50, sec1, StandardStructureTypes.H3, 0);
-
-    var structure = new PDStructureElement(StandardStructureTypes.SECT, sec1);
-
-    var section_one = formBuilder.drawElement(
-        new Cell("Left argument", new Color(229, 229, 229), Color.BLUE.darker(),
-            12, formBuilder.PAGE_WIDTH - 100, PDConstants.LEFT_ALIGN),
-        150, 100, 50, sec1, StandardStructureTypes.H4, 0);
-
-    var section_two = formBuilder.drawElement(
-        new Cell("Right argument", new Color(229, 229, 229), Color.BLUE.darker(),
-            12, formBuilder.PAGE_WIDTH - 100, PDConstants.LEFT_ALIGN),
-        400, 100, 50, sec1, StandardStructureTypes.H4, 0);
-
-    var numd = formBuilder.numDictionaries;
-
-    System.out.println(formBuilder.numDictionaries);
     formBuilder.saveAndClose("Accessible-Example.pdf");
   }
 }
